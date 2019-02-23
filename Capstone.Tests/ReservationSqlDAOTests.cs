d138754c4419c557aeed46a42178a761892775e5
Mariah Dawson <mlynndawson@gmail.com>
2019-02-23 09:22:51 -0500

    add a testMethod

--- Capstone.Tests/ReservationSqlDAOTests.cs
+++ Capstone.Tests/ReservationSqlDAOTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Capstone.DAL;
+using Capstone.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +10,27 @@ namespace Capstone.Tests
     [TestClass]
     public class ReservationSqlDAOTests : CapstoneDAOTests
     {
+        [TestMethod]
+        public void CreateReservationsTest_should_IncreaseCountBy1()
+        {
+            ReservationSqlDAO dao = new ReservationSqlDAO(ConnectionString);
+            int initalRowCount = GetRowCount("reservation");
 
+            Reservation res = new Reservation()
+            {
+                SiteId = SiteId,
+                Name = "C Sharp Reunion",
+                FromDate = Convert.ToDateTime("08-15-2019"),
+                ToDate = Convert.ToDateTime("08-20-2019")
+            };
+
+            dao.CreateReservation(res);
+
+            int finalRowCount = GetRowCount("reservation");
+            Assert.AreEqual(initalRowCount + 1, finalRowCount);
+
+        }
+        
+        
     }
 }
